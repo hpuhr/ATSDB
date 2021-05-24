f2fa8b9040b2207fdeed303741d330e5cd7202c0
alex-scp <jcrbalex@gmail.com>
2021-05-24 22:15:55 +0100

    check prerequisites

--- utils/aoc_vp.sh
+++ utils/aoc_vp.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#
+# This file is part of OpenATS COMPASS.
+#
+# COMPASS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# COMPASS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with COMPASS. If not, see <http://www.gnu.org/licenses/>.
+#
+
 PROGNAME=`basename $0`
 REF=
 REF_NAME=
@@ -36,7 +53,6 @@ while getopts "hd:s:r:R:t:T:" opt
   esac
 done
 
-
 if [ -z "$REF" -o -z "$TST" ]; then
    help
    exit 1
@@ -44,11 +60,21 @@ fi
 
 if [ ! -f "$REF" ]; then
    echo "reference run:$REF not found (bailing out...)"
-   exit 1
+   exit 2
 fi
 if [ ! -f "$TST" ]; then
    echo "test run:$TST not found (bailing out...)"
-   exit 1
+   exit 3
+fi
+
+if [ -z $(which aoc) ]; then
+   echo "AOC tool (aoc) is not installed or not accessible (bailing out...)"
+   exit 4
+fi
+
+if [ -z $(which aoc_vp.py) ]; then
+   echo "AOC viepoints generator tool (aoc_vp.py) not accessible (bailing out...)"
+   exit 5
 fi
 
 aoc -hlp $TIME_SHIFT -asf $PWD/$REF $PWD/$TST aoc_$DATE.res > aoc_$DATE.out
